78b56e9065e6407a028e34789cd0c208787a1edc
Danilo Naves <danilonaves2209@gmail.com>
2026-06-14 23:34:31 -0300

    feat: Configuração de gitpages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://fga-eps-mds.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/MeasureSoftGram-Docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'fga-eps-mds', // Usually your GitHub org/user name.
+  projectName: 'MeasureSoftGram-Docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
